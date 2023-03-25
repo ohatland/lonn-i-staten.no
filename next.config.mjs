bbaab53c772ee974e7157022a8c3b25ccf2b58f4
Øyvind Hatland <oyvind.hatland@yahoo.com>
2023-03-25 20:33:26 +0100

    use web vitals from axiom

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@
  * This is especially useful for Docker builds.
  */
 !process.env.SKIP_ENV_VALIDATION && (await import("./src/env.mjs"));
+import { withAxiom } from 'next-axiom';
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -19,4 +20,4 @@ const config = {
     defaultLocale: "en",
   },
 };
-export default config;
+export default withAxiom(config);
